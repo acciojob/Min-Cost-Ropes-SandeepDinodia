7bf23fee581b6ad9d3f1ad84f76da1b00089adbe
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-01-18 21:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function mincost(arr)
-{ 
+ 
 //write your code here
 // return the min cost
-  
-}
+	function mincost(arr) {
+    const minHeap = new MinHeap();
 
-module.exports=mincost;
+    // Add all ropes to the min heap
+    for (let i = 0; i < arr.length; i++) {
+        minHeap.push(arr[i]);
+    }
+
+    let totalCost = 0;
+
+    // Connect ropes until only one rope remains in the heap
+    while (minHeap.size() > 1) {
+        const rope1 = minHeap.pop();
+        const rope2 = minHeap.pop();
+
+        const currentCost = rope1 + rope2;
+        totalCost += currentCost;
+
+        minHeap.push(currentCost);
+    }
+
+    return totalCost;
+}
+  
+
